9baa39556d190cab8c948f93a3d2cccf851962dd
Tigerblue77 <37409593+tigerblue77@users.noreply.github.com>
2025-10-27 19:33:21 +0000

    Merge. Minor improvements and variables renaming

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -39,6 +39,18 @@ then
   fi
 fi
 
+# Check if HIGH_FAN_SPEED variable is in hexadecimal format. If not, convert it to hexadecimal
+if [[ "$HIGH_FAN_SPEED" == 0x* ]]
+then
+  readonly FAN_SPEED_INTERPOLATION_ENABLED=true
+
+  DECIMAL_HIGH_FAN_SPEED=$(convert_hexadecimal_value_to_decimal "$HIGH_FAN_SPEED")
+  HEXADECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
+else
+  DECIMAL_HIGH_FAN_SPEED="$HIGH_FAN_SPEED"
+  HEXADECIMAL_HIGH_FAN_SPEED=$(convert_decimal_value_to_hexadecimal "$HIGH_FAN_SPEED")
+fi
+
 set_iDRAC_login_string "$IDRAC_HOST" "$IDRAC_USERNAME" "$IDRAC_PASSWORD"
 
 get_Dell_server_model
@@ -63,12 +75,12 @@ echo "Server model: $SERVER_MANUFACTURER $SERVER_MODEL"
 echo "iDRAC/IPMI host: $IDRAC_HOST"
 
 # Log the fan speed objective, CPU temperature threshold and check interval
-echo "Line interpolation enable: $ENABLE_LINE_INTERPOLATION"
-if "$ENABLE_LINE_INTERPOLATION"
+echo "Fan speed interpolation enabled: $FAN_SPEED_INTERPOLATION_ENABLED"
+if "$FAN_SPEED_INTERPOLATION_ENABLED"
 then
   echo "Fan speed lower value: $DECIMAL_FAN_SPEED%"
   echo "Fan speed higher value: $DECIMAL_HIGH_FAN_SPEED%"
-  echo "CPU lower temperature threshold: $CPU_TEMPERATURE_FOR_START_LINE_INTERPOLATION°C"
+  echo "CPU lower temperature threshold: $CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION°C"
   echo "CPU higher temperature threshold: $CPU_TEMPERATURE_THRESHOLD°C"
 else
   echo "Fan speed objective: $DECIMAL_FAN_SPEED%"
@@ -133,44 +145,44 @@ while true; do
       IS_DELL_FAN_CONTROL_PROFILE_APPLIED=true
       COMMENT="CPU 2 temperature is too high, Dell default dynamic fan control profile applied for safety"
     fi
-  elif "$ENABLE_LINE_INTERPOLATION"
+  elif $FAN_SPEED_INTERPOLATION_ENABLED
   then
-    CURRENT_FAN_SPEED=$DECIMAL_FAN_SPEED
-    if [ $CPU1_TEMPERATURE -gt "$CPU_TEMPERATURE_FOR_START_LINE_INTERPOLATION" ] || [$IS_CPU2_TEMPERATURE_SENSOR_PRESENT] && [$CPU2_TEMPERATURE -gt "$CPU_TEMPERATURE_FOR_START_LINE_INTERPOLATION"];
+    DECIMAL_CURRENT_FAN_SPEED=$DECIMAL_FAN_SPEED
+
+    HIGHEST_CPU_TEMPERATURE=$CPU1_TEMPERATURE
+    if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
     then
-      CPU_HIGHER_TEMP=$CPU1_TEMPERATURE
-      if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-      then
-        if [ $CPU2_TEMPERATURE -gt $CPU1_TEMPERATURE ];
-        then
-          CPU_HIGHER_TEMP=$CPU2_TEMPERATURE
-        fi
+      if [ $CPU2_TEMPERATURE -gt $CPU1_TEMPERATURE ]; then
+        HIGHEST_CPU_TEMPERATURE=$CPU2_TEMPERATURE
       fi
+    fi
+    
+    if [ $HIGHEST_CPU_TEMPERATURE -gt "$CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION" ];
+    then
       #
       # F1 - lower fan speed
       # F2 - higher fan speed
-      # T_CPU - higher temperature from both CPUs (if only one exist that will be CPU1 temp value)
+      # T_CPU - highest temperature of both CPUs (if only one exists that will be CPU1 temp value)
       # T1 - lower temperature threshold
       # T2 - higher temperature threshold
       # Fan speed = F1 + ( ( F2 - F1 ) * ( T_CPU - T1 ) / ( T2 - T1 ) )
       #
-      # Difference between higher and lower temperature
-      TEMP_WINDOW="$((CPU_TEMPERATURE_THRESHOLD - CPU_TEMPERATURE_FOR_START_LINE_INTERPOLATION))"
-      # Temperature above lower value
-      TEMPERATURE_ABOVE_LOWER_THRESHOLD="$((CPU_HIGHER_TEMP - CPU_TEMPERATURE_FOR_START_LINE_INTERPOLATION))"
-      # Difference between higher and lower fan speed
-      FAN_WINDOW="$((DECIMAL_HIGH_FAN_SPEED - FAN_SPEED))"
+      # Temperature interpolation activation range
+      TEMPERATURE_INTERPOLATION_ACTIVATION_RANGE="$((CPU_TEMPERATURE_THRESHOLD - CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION))"
       FAN_VALUE_TO_ADD=0
-      # Check if TEMP_WINDOW is grater than 0
-      if [ $TEMP_WINDOW -gt $FAN_VALUE_TO_ADD ];
+      # Check if TEMPERATURE_INTERPOLATION_ACTIVATION_RANGE is > 0
+      if [ $TEMPERATURE_INTERPOLATION_ACTIVATION_RANGE -gt $FAN_VALUE_TO_ADD ];
       then
-        FAN_VALUE_TO_ADD="$((FAN_WINDOW * TEMPERATURE_ABOVE_LOWER_THRESHOLD / TEMP_WINDOW))"
+        # Temperature above lower value
+        TEMPERATURE_ABOVE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION="$((HIGHEST_CPU_TEMPERATURE - CPU_TEMPERATURE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION))"
+        # Difference between higher and lower fan speed
+        FAN_WINDOW="$((DECIMAL_HIGH_FAN_SPEED - DECIMAL_FAN_SPEED))"
+        FAN_VALUE_TO_ADD="$((FAN_WINDOW * TEMPERATURE_ABOVE_THRESHOLD_FOR_FAN_SPEED_INTERPOLATION / TEMPERATURE_INTERPOLATION_ACTIVATION_RANGE))"
       fi
-      CURRENT_FAN_SPEED="$((FAN_SPEED + FAN_VALUE_TO_ADD))"
+      DECIMAL_CURRENT_FAN_SPEED="$((DECIMAL_FAN_SPEED + FAN_VALUE_TO_ADD))"
     fi
-    # Convert decimal to hexadecimal value of fan speed
-    convert_current_fan_value_to_hexadecimal_format
-    apply_line_interpolation_fan_control_profile
+    HEXADECIMAL_CURRENT_FAN_SPEED=$(convert_decimal_value_to_hexadecimal $DECIMAL_CURRENT_FAN_SPEED)
+    apply_fan_speed_interpolation_fan_control_profile
   else
     apply_user_fan_control_profile
 
--- functions.sh
+++ functions.sh
@@ -14,10 +14,20 @@ function apply_user_fan_control_profile() {
   CURRENT_FAN_CONTROL_PROFILE="User static fan control profile ($DECIMAL_FAN_SPEED%)"
 }
 
-function apply_line_interpolation_fan_control_profile () {
+# This function applies a user-specified interpolated fan control profile
+function apply_fan_speed_interpolation_fan_control_profile () {
   ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 > /dev/null
   ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff $HEXADECIMAL_CURRENT_FAN_SPEED > /dev/null
-  CURRENT_FAN_CONTROL_PROFILE="Interpolated fan control profile ($CURRENT_FAN_SPEED%)"
+  CURRENT_FAN_CONTROL_PROFILE="Interpolated fan control profile ($DECIMAL_CURRENT_FAN_SPEED%)"
+}
+
+# Convert DECIMAL_NUMBER to hexadecimal
+# Usage : convert_decimal_value_to_hexadecimal $DECIMAL_NUMBER
+# Returns : hexadecimal value of DECIMAL_NUMBER
+function convert_decimal_value_to_hexadecimal () {
+  local DECIMAL_NUMBER=$1
+  local HEXADECIMAL_NUMBER=$(printf '0x%02x' $DECIMAL_NUMBER)
+  return $HEXADECIMAL_NUMBER
 }
 
 # Convert first parameter given ($DECIMAL_NUMBER) to hexadecimal
